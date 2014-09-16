ef559b0d6d45b885523ef516b098dfb0488ce7c5
Drew Lustro <drewlustro@gmail.com>
2014-09-16 01:35:46 -0400

    Wave with blue ball walkin the green

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,69 @@
+var xspacing = 16;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude = 75.0; // Height of wave
+var period = 500.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
+
+var canvasWidth = canvasHeight = 1000;
+var halfWidth = canvasWidth / 2;
+var halfHeight = canvasHeight / 2;
+var ringRadius = canvasWidth / 4;
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(canvasWidth, canvasHeight);
+  w = width+16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0);
+  fill();
+  noStroke();
+  calcWave();
+  renderWave();
+
+  noFill();
+  stroke(0, 240, 30);
+  ellipse(width/2, height/2, ringRadius * 2, ringRadius * 2);
+
+  walkBall();
+}
+
+function translateToCenter(obj) {
+  obj.translate(width / 2, height /2);
+}
+
+function walkBall() {
+  var x, y, now;
+  now = millis() / 1000;
+  x = cos(now) * ringRadius + halfWidth;
+  y = sin(now) * ringRadius + halfHeight;
+  noStroke();
+  fill(104, 102, 240);
+  ellipse(x, y, 50, 50);
+}
+
+function calcWave() {
+  // Increment theta (try different values for
+  // 'angular velocity' here
+  theta += 0.02;
+
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude;
+    x += dx;
   }
-  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function renderWave() {
+  noStroke();
+  fill(255);
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x * xspacing, height/2 + yvalues[x], 16, 16);
+  }
 }
